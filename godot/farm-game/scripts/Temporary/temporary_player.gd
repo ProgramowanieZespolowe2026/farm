116ee0b8835422f4025bcf0a3418fa88c10c8dbf
Jaros <filip.jarocki@wp.pl>
2026-01-28 13:35:12 +0100

    feat: add temporary assets/ tileMap/ player
    Temporary init:
    - add map scene
    - add player

---
+++ godot/farm-game/scripts/Temporary/temporary_player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+@onready var sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+   
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+	
+	
+	velocity = input_vector * speed
+	move_and_slide()
+	
+	if input_vector.x != 0:
+		sprite.flip_h = input_vector.x < 0
