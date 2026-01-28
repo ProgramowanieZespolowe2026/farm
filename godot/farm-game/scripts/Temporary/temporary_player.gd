cccec9b462614edbda00f7c7441a943d688531e5
Jaros <filip.jarocki@wp.pl>
2026-01-28 15:50:49 +0100

    update: temporary player
    Update input map keys and resize sprite

--- godot/farm-game/scripts/Temporary/temporary_player.gd
+++ godot/farm-game/scripts/Temporary/temporary_player.gd
@@ -7,9 +7,8 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
 	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
    
 	if input_vector.length() > 0:
 		input_vector = input_vector.normalized()
