ad8999c4ba646cc2e448202aecb31e7bcac7157e
Jaros <filip.jarocki@wp.pl>
2026-02-25 00:09:14 +0100

    feat: start add main plants

--- godot/farm-game/scripts/objects/plants/CropItemOrSeed.gd
+++ godot/farm-game/scripts/objects/plants/CropItemOrSeed.gd
@@ -1,5 +1,4 @@
 extends Sprite2D
 
-
-@export var item_name: String = "Tomato_item"
+@export var item_name: String = "Name"
 @export var item_value: int = 1
--- godot/farm-game/scripts/objects/plants/tomato/tomato_seed.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite2D
-
-
-@export var item_name: String = "Tomato_seed"
-@export var item_value: int = 3
