e16792d4fe17a749f2b00b91b9b76402d672698a
Jaros <filip.jarocki@wp.pl>
2026-04-20 12:38:40 +0200

    feat: add resize icon item logic

--- godot/farm-game/scripts/items/item.gd
+++ godot/farm-game/scripts/items/item.gd
@@ -1,28 +1,43 @@
 extends Node2D
 
-var item_name
-var item_value
+var item_name: String
+var item_value: int
 
-func _ready():
-	pass
+@onready var texture_rect: TextureRect = $TextureRect
+@onready var label: Label = $Label
 
-func set_item(nm, qt):
+func _ready() -> void:
+	texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+	texture_rect.stretch_mode = TextureRect.STRETCH_KEEP
+
+func set_item(nm: String, qt: int) -> void:
 	item_name = nm
 	item_value = qt
-	$TextureRect.texture = load("res://assets/items/Icons/" + item_name + ".png")
 	
-	var stack_size = int(JsonData.item_data[item_name]["StackSize"])
+	var tex: Texture2D = load("res://assets/items/Icons/" + item_name + ".png")
+	texture_rect.texture = tex
+	
+	if tex != null:
+		var tex_size = tex.get_size()
+		
+		if tex_size != Vector2(16, 16):
+			var scale_x: float = 16.0 / tex_size.x
+			var scale_y: float = 16.0 / tex_size.y
+			texture_rect.scale = Vector2(scale_x, scale_y)
+		else:
+			texture_rect.scale = Vector2(1, 1)
+	
+	var stack_size: int = int(JsonData.item_data[item_name]["StackSize"])
 	if stack_size == 1:
-		$Label.visible = false
+		label.visible = false
 	else:
-		$Label.visible = true
-		$Label.text = str(item_value)
+		label.visible = true
+		label.text = str(item_value)
 	
-	
-func add_item_value(amount_to_add):
+func add_item_value(amount_to_add: int) -> void:
 	item_value += amount_to_add
-	$Label.text = str(item_value)
+	label.text = str(item_value)
 	
-func decrease_item_value(amount_to_remove):
+func decrease_item_value(amount_to_remove: int) -> void:
 	item_value -= amount_to_remove
-	$Label.text = str(item_value)
+	label.text = str(item_value)
