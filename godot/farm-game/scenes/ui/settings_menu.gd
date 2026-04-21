1a05b840838cea6a205e1d32700ab0878e157544
kcieszynski-hub <cieszynski077@gmail.com>
2026-04-21 17:37:42 +0200

    feat: add settings menu
    Settings menu with:
    Music and SFX slider
    Music and SFX mute checkbox
    Fullscreen mode
    
    Works like a pause
    "P" to open
    "P"/ "ESC" / "Powrót" button to close

---
+++ godot/farm-game/scenes/ui/settings_menu.gd
@@ -0,0 +1,60 @@
+extends Control
+
+@onready var music_slider = $WoodenPanel/MarginContainer/Control/AudioControl
+@onready var sfx_slider = $WoodenPanel/MarginContainer/Control/SFXControl
+@onready var music_mute = $WoodenPanel/MarginContainer/Control/MusicMuteCheckbox
+@onready var sfx_mute = $WoodenPanel/MarginContainer/Control/SFXMuteCheckbox
+
+var music_bus
+var sfx_bus
+
+func _ready():
+	visible = false
+	music_bus = AudioServer.get_bus_index("Music")
+	sfx_bus = AudioServer.get_bus_index("Sfx")
+
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
+
+	music_mute.button_pressed = AudioServer.is_bus_mute(music_bus)
+	sfx_mute.button_pressed = AudioServer.is_bus_mute(sfx_bus)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("open_settings"):
+		if visible:
+			close()
+		else:
+			open()
+	elif event.is_action_pressed("ui_cancel") and visible:
+			close()
+
+func open():
+	visible = true
+	get_tree().paused = true
+
+func close():
+	visible = false
+	get_tree().paused = false
+
+func _on_audio_control_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
+
+func _on_sfx_control_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
+
+func _on_music_mute_checkbox_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(music_bus, toggled_on)
+
+func _on_sfx_mute_checkbox_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(sfx_bus, toggled_on)
+	
+func _on_back_to_menu_pressed():
+	close()
+	
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	print("Fullscreen toggled: ", toggled_on)
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
